59006205b0be7eb03f164b586b6a7a2e34f19bb1
Vladislav Maslov <qwertyuniverse12@yandex.ru>
2022-10-06 15:21:46 +0300

    feat: add donate, testimonials popup and menu scripts

--- online-zoo/pages/donate/script.js
+++ online-zoo/pages/donate/script.js
@@ -1,14 +1,76 @@
-console.log(1);
-let label = document.querySelectorAll(".radio__label");
-for (key of label) {
-    key.addEventListener("click", showBorder);
+//Скрипт меню
+document.querySelector(".menu__img").addEventListener("click", showMenu);
+document.querySelector(".close").addEventListener("click", showMenu);
+document.querySelector(".menu .darkest-block").addEventListener("click", showMenu);
+
+function showMenu() {
+    if (document.querySelector(".menu__nav").classList.contains("display-none")) {
+      document.querySelector(".menu__nav").classList.remove("display-none");
+      document.querySelector(".menu .darkest-block").classList.remove("display-none");
+    setTimeout(() => {
+      document.querySelector(".menu__nav").classList.remove("opacity-none");
+      document.querySelector(".menu .darkest-block").classList.remove("opacity-none");
+    },10);
+    } else {
+      document.querySelector(".menu__nav").classList.add("opacity-none");
+      document.querySelector(".menu .darkest-block").classList.add("opacity-none");
+    setTimeout(() => {
+      document.querySelector(".menu__nav").classList.add("display-none");
+      document.querySelector(".menu .darkest-block").classList.add("display-none");
+    },500);
+    }      
 }
 
-console.log(label);
 
-function showBorder() {
-    for (key of label) {
+
+
+
+//Скрипт для изменения значения инпута при нажатии на сумму
+let labels = document.querySelectorAll(".radio__label");
+let input = document.querySelector(".donate__input");
+for (key of labels) {
+    key.addEventListener("click", changeInput);
+}
+
+
+//Изменение значения в input при нажатии на label и изменение рамки
+function changeInput() {
+    input.value = this.children[0].value;
+    input.setAttribute("value", this.children[0].value);
+    changeBorder(this);
+}
+
+//Рамка для label
+function changeBorder(label) {
+    for (key of labels) {
         key.classList.remove("label__border");
     }
-    this.classList.add("label__border");
+    label.classList.add("label__border");
 }
+
+
+//Скрипт изменения радиобаттонов при введении значения в инпут
+input.addEventListener("input", changeRadio);
+
+let radioButtons = document.querySelectorAll(".radio");
+
+function changeRadio(e) {
+    if (e.target.value.length > 4) {
+        e.target.value = e.target.value.slice(0, e.target.value.length-1);
+        return;
+    }
+    input.setAttribute("value", e.target.value);
+    let radioValue = Array.from(radioButtons).find((item) => {
+        return (e.target.value == item.value);
+    }); //Ищем, есть ли введенное значение в списке
+    if (radioValue) {
+        radioValue.checked = true;
+        changeBorder(radioValue.parentElement);
+    } else {
+        for (key of radioButtons) {
+            key.parentElement.classList.remove("label__border");
+            key.checked = false;
+        }
+    }
+}
+
---
+++ online-zoo/pages/main/script.js
@@ -0,0 +1,49 @@
+//Скрипт меню
+document.querySelector(".menu__img").addEventListener("click", showMenu);
+document.querySelector(".close").addEventListener("click", showMenu);
+document.querySelector(".menu .darkest-block").addEventListener("click", showMenu);
+
+function showMenu() {
+    if (document.querySelector(".menu__nav").classList.contains("display-none")) {
+      document.querySelector(".menu__nav").classList.remove("display-none");
+      document.querySelector(".menu .darkest-block").classList.remove("display-none");
+    setTimeout(() => {
+      document.querySelector(".menu__nav").classList.remove("opacity-none");
+      document.querySelector(".menu .darkest-block").classList.remove("opacity-none");
+    },10);
+    } else {
+      document.querySelector(".menu__nav").classList.add("opacity-none");
+      document.querySelector(".menu .darkest-block").classList.add("opacity-none");
+    setTimeout(() => {
+      document.querySelector(".menu__nav").classList.add("display-none");
+      document.querySelector(".menu .darkest-block").classList.add("display-none");
+    },500);
+    } 
+}
+
+
+
+//Скрипт попап карточки
+let testimonials = document.querySelectorAll(".testimonials__card");
+for (let i = 0; i < testimonials.length; i++) {
+   testimonials[i].addEventListener("click", showPopup);
+}
+
+//открытие
+function showPopup() {
+    if (document.documentElement.clientWidth > 999) return;
+    this.classList.add("testimonials__card-popup");
+    document.querySelector(".testimonials__card-bg").classList.remove("display-none");
+}
+
+//закрытие
+document.querySelector(".testimonials__card-bg").addEventListener("click", closePopup);
+document.querySelector(".close-popup").addEventListener("click", closePopup);
+
+function closePopup() {
+    Array.from(testimonials).find((item) => {
+        return item.classList.contains("testimonials__card-popup");
+    }).classList.remove("testimonials__card-popup");
+    document.querySelector(".testimonials__card-bg").classList.add("display-none");
+}
+
