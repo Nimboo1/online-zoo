ffa3808b87a9b51f001ba46a6097bb8102029f62
Vladislav Maslov <qwertyuniverse12@yandex.ru>
2022-10-09 18:04:51 +0300

    feat: add slider script

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -24,7 +24,10 @@ function showMenu() {
 
 
 //Скрипт попап карточки
+let card;
 let testimonials = document.querySelectorAll(".testimonials__card");
+let bg = document.querySelector(".testimonials__card-bg");
+let darkbg = document.querySelector(".testimonials__card-darkbg");
 for (let i = 0; i < testimonials.length; i++) {
    testimonials[i].addEventListener("click", showPopup);
 }
@@ -32,19 +35,23 @@ for (let i = 0; i < testimonials.length; i++) {
 //открытие
 function showPopup() {
     if (document.documentElement.clientWidth > 999) return;
-    this.classList.add("testimonials__card-popup");
-    document.querySelector(".testimonials__card-bg").classList.remove("display-none");
+    card = this.cloneNode(true);
+    card.classList.add("testimonials__card-popup");
+    darkbg.classList.remove("display-none");
+    bg.append(card);
+    if (bg.offsetHeight > darkbg.offsetHeight) {
+      bg.style.marginTop = bg.offsetHeight-darkbg.offsetHeight + 20 + "px";
+    }
 }
 
 //закрытие
-document.querySelector(".testimonials__card-bg").addEventListener("click", closePopup);
+darkbg.addEventListener("click", closePopup);
 document.querySelector(".close-popup").addEventListener("click", closePopup);
 
 function closePopup() {
-    Array.from(testimonials).find((item) => {
-        return item.classList.contains("testimonials__card-popup");
-    }).classList.remove("testimonials__card-popup");
-    document.querySelector(".testimonials__card-bg").classList.add("display-none");
+    darkbg.classList.add("display-none");
+    bg.style.marginTop = "0";
+    card.remove();
 }
 
 
@@ -62,4 +69,123 @@ function testimonialsShift() {
   let shift = (document.documentElement.clientWidth < desktopWidth) ? 293+30 : 268+29;
   shift *= this.value;
   testimonialsContainer.style.transform = "translate("+ -shift +"px)";
+}
+
+
+
+
+//Скрипт слайдера отзывов
+const btnLeft = document.querySelector(".slider__button-left");
+const btnRight = document.querySelector(".slider__button-right");
+
+btnLeft.addEventListener("click", sliderShiftLeft);
+btnRight.addEventListener("click", sliderShiftRight);
+let flag = true;
+
+
+//Перелистывание влево
+function sliderShiftLeft() {
+  if (!flag) return;
+  flag = false;
+
+  createSlide("left");
+
+  let rowContainers = document.querySelectorAll(".row__container");
+  console.log(rowContainers);
+
+  for (key of rowContainers) {
+    key.classList.add("container-transition");
+  }
+  setTimeout(() => {
+    for (key of rowContainers) {
+      key.classList.add("container-transformleft");
+    }
+  },50);
+  console.log(rowContainers);
+
+  setTimeout(() => {
+    rowContainers[0].remove();
+    rowContainers[1].classList.remove("container-transition");
+    rowContainers[1].classList.remove("container-transformleft");
+    flag = true;
+  }, 700);
+}
+
+//Перелистывание вправо
+function sliderShiftRight() {
+  if (!flag) return;
+  flag = false;
+
+  createSlide("right");
+
+  let rowContainers = document.querySelectorAll(".row__container");
+
+  for (key of rowContainers) {
+    key.classList.add("container-transformleft");
+  }
+
+  setTimeout(() => {
+    for (key of rowContainers) {
+      key.classList.add("container-transition");
+    }
+    rowContainers[0].classList.add("container-transformright");
+    rowContainers[1].classList.add("container-transformright");
+  }, 50);
+
+
+
+  setTimeout(() => {
+    rowContainers[0].classList.remove("container-transition");
+    rowContainers[0].classList.remove("container-transformleft");
+    rowContainers[0].classList.remove("container-transformright");
+    rowContainers[1].remove();
+    flag = true;
+  }, 700);
+}
+
+//Создание нового слайда
+const arrForShuffle = [0,1,2,3,4,5,6,7];
+let sliderItems = document.querySelectorAll(".slider__item");
+let containerWrapper = document.querySelector(".row__container-wrapper");
+
+function createSlide(direction) {
+  let rowContainer = document.createElement("div");
+  rowContainer.classList.add("row__container");
+  let sliderRow1 = document.createElement("div");
+  sliderRow1.classList.add("slider__row");
+  let sliderRow2 = document.createElement("div");
+  sliderRow2.classList.add("slider__row");
+
+  let shuffledArr = shuffleArr(arrForShuffle);
+  for (let i = 0; i < 3; i++) {
+    sliderRow1.append(sliderItems[shuffledArr[i]].cloneNode(true));
+  } 
+  for (let i = 3; i < 6; i++) {
+    sliderRow2.append(sliderItems[shuffledArr[i]].cloneNode(true));
+  } 
+
+  rowContainer.append(sliderRow1);
+  rowContainer.append(sliderRow2);
+
+  if (direction == "left") {
+    containerWrapper.append(rowContainer);
+  } else {
+    containerWrapper.prepend(rowContainer);
+  }
+}
+
+
+//Перемешивание массива дл рандомизации порядка картинок
+function shuffleArr(arr) {
+  let i = arr.length;
+  let j = 0;
+  let temp;
+
+  while (i--) {
+    j = Math.floor(Math.random() * (i+1));
+    temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  return arr;
 }
